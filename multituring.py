c1f1c0a05e28f2e9e554db7a34e74cba67fb289b
Jacek Wielemborek <d33tah@gmail.com>
2014-06-02 14:06:03 +0200

    Add support for multiple tapes.

--- multituring.py
+++ multituring.py
@@ -1,29 +1,39 @@
 import collections
 import copy
+import sys
+
+if sys.version >= '3':
+    raw_input = input
 
 class Multituring:
 
     def __init__(self, mt, tape_start):
         self.mt = mt
-        self.tape = dict(enumerate(tape_start))
+        self.num_tapes = mt.get('num_tapes', 1)
+        self.tapes = ([dict(enumerate(tape_start))] +
+                      [{} for i in range(self.num_tapes-1)])
         self.current_state = self.mt['start']
         self.empty_symbol = self.mt['empty_symbol']
-        self.pos = 0
-        self.direction = 'S'
+        self.tapes_pos = [0 for i in range(self.num_tapes)]
+        self.directions = 'S' * self.num_tapes
         self.symbol = self.empty_symbol
 
-    def _get_tape_contents(self, sep='', highlight_pos=False, offset=0):
-        our_tape = copy.deepcopy(self.tape)
-        if self.pos not in our_tape:
-            our_tape[self.pos] = self.empty_symbol
+    def _get_tape_contents(self, tape_number=0, sep='', highlight_pos=False,
+                           left_offset=0, right_offset=None):
+        if right_offset is None:
+            right_offset = left_offset
+        our_tape = copy.deepcopy(self.tapes[tape_number])
+        if self.tapes_pos[tape_number] not in our_tape:
+            our_tape[self.tapes_pos[tape_number]] = self.empty_symbol
         if highlight_pos:
-            our_tape[self.pos] = "\033[4m%s\033[0m" % our_tape[self.pos]
+            our_tape[self.tapes_pos[tape_number]] = ("\033[4m%s\033[0m" %
+                our_tape[self.tapes_pos[tape_number]])
 
         if our_tape != {}:
             min_key = min(our_tape.keys())
             max_key = max(our_tape.keys())
             our_tape_contents= []
-            for i in range(min_key-offset, max_key+offset+1):
+            for i in range(min_key-left_offset, max_key+right_offset+1):
                 if i in our_tape:
                     our_tape_contents += [our_tape[i]]
                 else:
@@ -37,46 +47,57 @@ class Multituring:
 
     def print_state(self):
 
-        print('(%s,\t%s,\t%s)' % (self.current_state, self.symbol,
-                                  self.direction))
-        print(self._get_tape_contents(sep=' ', highlight_pos=True, offset=3))
+        print('(%s,\t%s,\t%s,\t%s)' % (self.current_state, self.symbol,
+                                  self.directions, self.tapes_pos))
+        for tape_number in range(len(self.tapes)):
+            print(self._get_tape_contents(tape_number, sep=' ',
+                                          highlight_pos=True, left_offset=3))
         print("")
+
+    def _step(self, print_progress):
+        self.symbol = ""
+        for i in range(len(self.tapes)):
+            self.symbol += self.tapes[i].get(self.tapes_pos[i],
+                                             self.empty_symbol)
+
+        if print_progress:
+            self.print_state()
+
+        if self.current_state in self.mt['stop']:
+            return True
+
+        if not self.current_state in self.mt['tab']:
+            raise Exception('Entered an unknown state: %s'
+                            % self.current_state)
+
+        if not self.symbol in self.mt['tab'][self.current_state]:
+            error = 'Symbol not defined for state %s: %s' % (
+                        self.current_state, self.symbol)
+            raise Exception(error)
+
+        turing_tuple = self.mt['tab'][self.current_state][self.symbol]
+        self.current_state = turing_tuple[0]
+        self.symbol = turing_tuple[1]
+        self.directions = turing_tuple[2]
+        for i in range(len(self.directions)):
+
+            if self.directions[i] == 'R':
+                offset = 1
+            elif self.directions[i] == 'L':
+                offset = -1
+            else:
+                assert(self.directions[i] == 'S')
+                offset = 0
+
+            self.tapes[i][self.tapes_pos[i]] = self.symbol[i]
+            self.tapes_pos[i] += offset
 
     def run(self, print_progress=True):
 
         while True:
-            self.symbol = self.tape.get(self.pos, self.empty_symbol)
-
-            if print_progress:
-                self.print_state()
-
-            if self.current_state in self.mt['stop']:
-                return True
-
-            if not self.current_state in self.mt['tab']:
-                raise Exception('Entered an unknown state: %s'
-                                % self.current_state)
-
-            if not self.symbol in self.mt['tab'][self.current_state]:
-                error = 'Symbol not defined for state %s: %s' % (
-                            self.current_state, self.symbol)
-                raise Exception(error)
-
-            turing_tuple = self.mt['tab'][self.current_state][self.symbol]
-            self.current_state = turing_tuple[0]
-            self.symbol = turing_tuple[1]
-            self.direction = turing_tuple[2]
-
-            if self.direction == 'R':
-                offset = 1
-            elif self.direction == 'L':
-                offset = -1
-            else:
-                assert(self.direction == 'S')
-                offset = 0
-
-            self.tape[self.pos] = self.symbol
-            self.pos += offset
+            ret = self._step(print_progress)
+            if ret:
+                return ret
 
 def interactive_test(mt):
     tape_start = raw_input("Enter the tape contents: ")
